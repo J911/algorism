745dfc54f1832679ef73aba13fad95c17982d386
Jay <fb.jaemin@gmail.com>
2018-05-08 10:37:21 +0900

    update memo_fibonacci method

--- DynamicProgramming/fibonacci.js
+++ DynamicProgramming/fibonacci.js
@@ -7,8 +7,20 @@ const Recursive_Fibonacci = n => {
     if(n == 1 || n == 2) return 1
     return (Recursive_Fibonacci(n-1) + Recursive_Fibonacci(n-2))
 }
-
+const Memo_Fibonacci = (n, f=[]) => {
+    if(f.length == 0){
+        for(i=0;i<n;i++) f.push(0)
+        n--
+    }
+    if(f[n] !== 0) return f[n]
+    else {
+        if(n == 0 || n == 1) f[n] = 1
+        else f[n] = Memo_Fibonacci(n-1,f) + Memo_Fibonacci(n-2,f)
+        return f[n]
+    }
+}
 module.exports = {
     DP_Fibonacci,
-    Recursive_Fibonacci
+    Recursive_Fibonacci,
+    Memo_Fibonacci
 }
--- DynamicProgramming/test_Fibonacci.js
+++ DynamicProgramming/test_Fibonacci.js
@@ -1,9 +1,24 @@
 const fibonacci = require('./fibonacci')
 
+let DP_Fibonacci = []
+let Recursive_Fibonacci = []
+let Memo_Fibonacci = []
+
+const loop = (n, fn) => {
+    for(i=1;i<n;i++) fn(i)
+}
 console.time('DP_Fibonacci')
-console.log(fibonacci.DP_Fibonacci(40))
+loop(11, i => DP_Fibonacci.push(fibonacci.DP_Fibonacci(i)))
+console.log(DP_Fibonacci)
 console.timeEnd('DP_Fibonacci')
 
 console.time('Recursive_Fibonacci')
-console.log(fibonacci.Recursive_Fibonacci(40))
+loop(11, i => Recursive_Fibonacci.push(fibonacci.Recursive_Fibonacci(i)))
+console.log(Recursive_Fibonacci)
 console.timeEnd('Recursive_Fibonacci')
+
+
+console.time('Memo_Fibonacci')
+loop(11, i => Memo_Fibonacci.push(fibonacci.Memo_Fibonacci(i)))
+console.log(Memo_Fibonacci)
+console.timeEnd('Memo_Fibonacci')
