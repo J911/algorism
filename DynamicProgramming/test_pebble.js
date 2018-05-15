321790a4ff84343ee9b718d942004d8d253d1ac7
Jay <fb.jaemin@gmail.com>
2018-05-15 11:24:16 +0900

    update test case

--- DynamicProgramming/test_pebble.js
+++ DynamicProgramming/test_pebble.js
@@ -1,8 +1,8 @@
 const pebble = require('./pebble')
 let p = new pebble([
-    [1,1,1,1,1],
-    [1,1,1,1,1],
-    [1,1,1,1,1],
+    [6,7,12,-5,5,3,11,3],
+    [-8,10,14,9,7,13,8,5],
+    [11,12,7,4,8,-2,9,4],
 ])
 
 console.log('pebble Max:',p.max)
